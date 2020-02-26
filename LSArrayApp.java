28a1375d20df4bc3c75bff444f9418362aa56b15
Yelanika <yelanika.gunasekara@gmail.com>
2020-02-27 01:13:58 +0200

    printHours_complete

--- LSArrayApp.java
+++ LSArrayApp.java
@@ -41,15 +41,15 @@ class LSBaseApp extends LSDataApp
 /*	int stage = -1;
 	int day = -1;
 	int startTime = -1;*/
-	String[] lsData = new String[2];
+	String[] lsData = new String[10];
 //	int zone = -1;
 	int[] stageArr = new int[2976];
 	int[] dayArr = new int[2976];
 	int[] timeArr = new int[2976];
-	int[] zoneArr = new int[2976];
-	String[] zones = new String[8];
+//	int[] zoneArr = new int[2976];
+//	String[] zones = new String[8];
 
-	String ExtractAndPrintData()
+	String ExtractData()
 	{
 		try {
 			Scanner sc = new Scanner(new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt"));
@@ -72,46 +72,44 @@ class LSBaseApp extends LSDataApp
 		return "";
 	}
 
+
 	String printAllAreas()
 	{
 
 //		String[] lsData = new String[2];
 		for (int i=0; i <2976; i++)
 		{
-			int k = 0;
 			String t ="";
-			lsData = ls[i].split("_");
 
-			stageArr[i] = Integer.parseInt(lsData[0]);
-			dayArr[i] = Integer.parseInt(lsData[1]);
-//			int temp1 = Integer.parseInt(lsData[2]);
+			lsData = ls[i].split("[_\\D\\s]+");    //D-split at every non-digit
 
-			zones = lsData[2].split("[\\D\\s]+");    //D-split at every non-digit
-			timeArr[i] = Integer.parseInt(zones[0]);
+			stageArr[i] = Integer.parseInt(lsData[0]);		//lsData[0]
+			dayArr[i] = Integer.parseInt(lsData[1]);		//lsData[1]
+			timeArr[i] = Integer.parseInt(lsData[2]);		//lsData[2]
 
-			t = "stage: "  +  stageArr[i] + "	day: "	+ dayArr[i] +  "	time: " + zones[0] + "h00"; 
-			t = t + "	areas: " + zones[1];
+			t = "stage: "  +  stageArr[i] + "	day: "	+ dayArr[i] +  "	time: " + lsData[2] + "h00"; 
+			t = t + "	areas: " + lsData[3];
 
 			if (stageArr[i] == 2) {
-				t= t + "  " + zones[2];
+				t= t + "  " + lsData[4];
 			}
 			if (stageArr[i] == 3) {
-				t= t+  "  " + zones[2] + "  " + zones[3];
+				t= t+  "  " + lsData[4] + "  " + lsData[5];
 			}
 //			if (stageArr[i] == 4) {
-//				t=t +  "  " + zones[2] + "  " + zones[3] + "  " + zones[4];
+//				t=t +  "  " + zones[4 + "  " + zones[5] + "  " + zones[4];
 //			}
 			if (stageArr[i] == 5) {
-				t=t +  "  " + zones[2] + "  " + zones[3] + "  " + zones[4] + "  " + zones[5];
+				t=t +  "  " + lsData[4] + "  " + lsData[5] + "  " + lsData[6] + "  " + lsData[7];
 			}
 			if (stageArr[i] == 6) {
-				t=t +  "  " + zones[2] + "  " + zones[3] + "  " + zones[4] + "  " + zones[5] + "  " + zones[6];
+				t=t +  "  " + lsData[4] + "  " + lsData[5] + "  " + lsData[6] + "  " + lsData[7] + "  " + lsData[8];
 			}
 			if (stageArr[i] == 7) {
-				t=t +  "  " + zones[2] + "  " + zones[3] + "  " + zones[4] + "  " + zones[5] + "  " + zones[6] + "  " + zones[7];
+				t=t +  "  " + lsData[4] + "  " + lsData[5] + "  " + lsData[6] + "  " + lsData[7] + "  " + lsData[8] + "  " + lsData[9];
 			}
 			if (stageArr[i] == 8) {
-				t=t +  " " + zones[2] + "  " + zones[3] + "  " + zones[4] + "  " + zones[5] + "  " + zones[6] + "  " + zones[7] + "  " + zones[8];
+				t=t +  "  " + lsData[4] + "  " + lsData[5] + "  " + lsData[6] + "  " + lsData[7] + "  " + lsData[8] + "  " + lsData[9] + "  " + lsData[10];
 			}
 			System.out.println(t);
 
@@ -121,18 +119,61 @@ class LSBaseApp extends LSDataApp
 		return "";
 	}
 
-	String Search(int stage, int day, int startTime)
+	String printAreas(int stage, int day, int startTime)
 	{
 		String tempString = "";
+		int add = 0;
 
 		for (int j =0; j < 2976; j++)
 		{
-			if ((lsData[0] == String.valueOf(stage)) && (lsData[1] == String.valueOf(day)) && (lsData[2] == String.valueOf(startTime)))
+			lsData = ls[j].split("[_\\D\\s]+");
+
+			if ((stage == Integer.parseInt(lsData[0])) && (day == Integer.parseInt(lsData[1])) && (startTime == Integer.parseInt(lsData[2])))
 			{
-				tempString = "3456789012";
-			}
+				if (stage == 1)
+				{
+					tempString = lsData[3];
+				}
+				else if (stage == 2)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4];
+				}
+				else if (stage == 3)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5];
+				}
+				else if (stage == 4)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5] + "  &  " + lsData[6];
+				}
+				else if (stage == 5)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5] + "  &  " + lsData[6] + "  &  " + lsData[7];
+				}
+				else if (stage == 6)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5] + "  &  " + lsData[6] + "  &  " + lsData[7] + "  &  " + lsData[8];
+				}
+				else if (stage == 7)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5] + "  &  " + lsData[6] + "  &  " + lsData[7] + "  &  " + lsData[8] + "  &  " + lsData[9];
+				}
+				else if (stage == 8)
+				{
+					tempString = lsData[3] + "  &  " + lsData[4] + "  &  " + lsData[5] + "  &  " + lsData[6] + "  &  " + lsData[7] + "  &  " + lsData[8] + "  &  " + lsData[9] + "  &  " + lsData[10];
+				}add = add + 1;
+			}/*else
+			{
+				tempString = tempString + " none found";
+			}*/
 		}
-		System.out.println(tempString);
+		if (add==0)
+		{
+			tempString = "none found";
+		}
+
+		System.out.println("Areas: " + tempString);
+//		return tempString;
 		return "";
 	}
 }
@@ -146,18 +187,19 @@ public class LSArrayApp extends LSBaseApp
 	{
 
 		LSArrayApp lsObj = new LSArrayApp();
-		lsObj.ExtractAndPrintData();
+		lsObj.ExtractData();
 
 		int tempStage = Integer.parseInt(args[0]);
-		System.out.println(tempStage);
+//		System.out.println(tempStage);
 
 		int tempDay = Integer.parseInt(args[1]);
-		System.out.println(tempDay);
+//		System.out.println(tempDay);
 
 		int tempStartTime = Integer.parseInt(args[2]);
-		System.out.println(tempStartTime);
+//		System.out.println(tempStartTime);
 
-		lsObj.printAllAreas();
+//		lsObj.printAllAreas();
+		lsObj.printAreas(tempStage,tempDay,tempStartTime);
 
 	}
 }
